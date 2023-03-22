f17d01103a69c5307b1942c02b1c73b53868e7ff
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2023-03-22 22:18:07 +0530

    First commit

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_URL_SIZE 1000
+#define MAX_CMD_SIZE 1008
+
+void search(char *search_words[], int word_count){
+	
+	if (!word_count){
+		printf("Too few search terms");
+		return;
+	} 
+
+	char url[MAX_URL_SIZE] = "";
+  char url_base[] = "https://www.bing.com/search?q=";
+  char cmd[MAX_CMD_SIZE] = "start ";
+
+  strcat_s(url, MAX_URL_SIZE * sizeof(char), url_base);
+
+  strcat_s(url, MAX_URL_SIZE * sizeof(char), search_words[0]);
+
+  for (int i = 1; i < word_count; i++) {
+    strcat_s(url, MAX_URL_SIZE * sizeof(char), "+");
+    strcat_s(url, MAX_URL_SIZE * sizeof(char), search_words[i]);
+	}
+
+  strcat_s(cmd, MAX_CMD_SIZE, url);
+
+	system(cmd);
+}
+
+int main(int argc, char *args[]) {
+
+	int word_count = argc - 1;
+	char *search_words[word_count];
+
+	for (int i=0; i<word_count; i++) {
+		search_words[i]	= args[i+1];
+	}
+
+	search(search_words, word_count);
+
+}

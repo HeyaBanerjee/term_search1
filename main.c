dc3f03aded6a465a174c793487ae2e85a0162e4b
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2023-03-23 23:45:22 +0530

    Fixed a buffer issue

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 #define MAX_URL_SIZE 1000
 #define MAX_CMD_SIZE 1008
 
-void search(char *search_words[], int word_count){
-	
-	if (!word_count){
-		printf("Too few search terms");
-		return;
-	} 
+void search(char *search_words[], int word_count) {
 
-	char url[MAX_URL_SIZE] = "";
+  if (!word_count) {
+    printf("Too few search terms");
+    return;
+  }
+
+  char url[MAX_URL_SIZE] = "";
   char url_base[] = "https://www.bing.com/search?q=";
   char cmd[MAX_CMD_SIZE] = "start ";
 
@@ -23,22 +23,21 @@ void search(char *search_words[], int word_count){
   for (int i = 1; i < word_count; i++) {
     strcat_s(url, MAX_URL_SIZE * sizeof(char), "+");
     strcat_s(url, MAX_URL_SIZE * sizeof(char), search_words[i]);
-	}
+  }
 
-  strcat_s(cmd, MAX_CMD_SIZE, url);
+  strcat_s(cmd, MAX_CMD_SIZE * sizeof(char), url);
 
-	system(cmd);
+  system(cmd);
 }
 
 int main(int argc, char *args[]) {
 
-	int word_count = argc - 1;
-	char *search_words[word_count];
+  int word_count = argc - 1;
+  char *search_words[word_count];
 
-	for (int i=0; i<word_count; i++) {
-		search_words[i]	= args[i+1];
-	}
+  for (int i = 0; i < word_count; i++) {
+    search_words[i] = args[i + 1];
+  }
 
-	search(search_words, word_count);
-
+  search(search_words, word_count);
 }
